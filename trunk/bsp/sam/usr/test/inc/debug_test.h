1d7f9233606fb6123c820624a5e42d53b7937d22
maik42 <maik42>
2012-07-29 15:58:42 +0000

    see CHANGELOG

---
+++ trunk/bsp/sam/usr/test/inc/debug_test.h
@@ -0,0 +1,15 @@
+/*
+ * debug_test.h
+ *
+ *  Created on: 11.09.2010
+ *      Author: maik
+ */
+
+#ifndef DEBUG_TEST_H_
+#define DEBUG_TEST_H_
+
+#include <mfs/vfs.h>
+
+extern void debug_test_install (MFS_descriptor_t * test);
+
+#endif /* DEBUG_TEST_H_ */
---
+++ trunk/bsp/sam/usr/test/inc/digio_test.h
@@ -0,0 +1,13 @@
+/*
+ *  Copyright (C) 2004-2006  Michael Feiner
+ *
+ */
+
+#ifndef DIGIO_TEST_H
+#define DIGIO_TEST_H
+
+#include <mfs/vfs.h>
+
+extern void digio_test_install (MFS_descriptor_t * test);
+
+#endif
